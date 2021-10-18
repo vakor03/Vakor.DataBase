ecbe5c17d997345ec20c82ad552ca460d57926f5
Vakor <myksv200@gmail.com>
2021-10-18 14:42:09 +0300

    19.10 Record

---
+++ Vakor.DataBase/Vakor.DataBase.Lib/ObjectAreas/RecordBlocks/Records/IRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace Vakor.DataBase.Lib.ObjectAreas.RecordBlocks.Records
+{
+    public interface IRecord
+    {
+        public int Key { get; }
+        public string Data { get; set; }
+
+        public string GenerateString();
+    }
+}
---
+++ Vakor.DataBase/Vakor.DataBase.Lib/ObjectAreas/RecordBlocks/Records/Record.cs
@@ -0,0 +1,26 @@
+﻿namespace Vakor.DataBase.Lib.ObjectAreas.RecordBlocks.Records
+{
+    public class Record : IRecord
+    {
+        public int Key { get; }
+        public string Data { get; set; }
+        
+        public Record(int key, string data)
+        {
+            Key = key;
+            Data = data;
+        }
+
+        public Record(string recordString)
+        {
+            Key = int.Parse(recordString.Substring(0,recordString.IndexOf(';')));
+            Data = recordString.Substring(recordString.IndexOf(';') + 1);
+        }
+        
+        public string GenerateString()
+        {
+            string result = $"{Key};{Data}";
+            return result;
+        }
+    }
+}
