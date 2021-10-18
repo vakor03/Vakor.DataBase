8360bd0c1b000474265107be8b447226fb5b43fb
Vakor <myksv200@gmail.com>
2021-10-18 14:42:50 +0300

    18.10 Configuration

---
+++ Vakor.DataBase/Vakor.DataBase.Lib/Configurations/IConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace Vakor.DataBase.Lib.Configurations
+{
+    public interface IConfiguration
+    {
+        public const string FileName = ".config";
+        int BlockCapacity { get; set; }
+        int BlocksCount { get; set; }
+        int LastIndex { get; set; }
+        double FillIndex { get; }
+
+        string[] GenerateOutputData();
+        void ChangeMaxCapacity(double extendCapacity);
+    }
+}
