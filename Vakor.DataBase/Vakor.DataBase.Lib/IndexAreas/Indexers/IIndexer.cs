945f6ad79f797daada78c14cf32ff13363235b73
Vakor <myksv200@gmail.com>
2021-10-18 14:43:01 +0300

    18.10 Indexer

---
+++ Vakor.DataBase/Vakor.DataBase.Lib/IndexAreas/Indexers/IIndexer.cs
@@ -0,0 +1,11 @@
+﻿namespace Vakor.DataBase.Lib.IndexAreas.Indexers
+{
+    public interface IIndexer
+    {
+        public int StartKeyValue { get; set; }
+        public int EndKeyValue { get; set; }
+        public int ObjectBlockId { get; set; }
+
+        public string GenerateString();
+    }
+}
---
+++ Vakor.DataBase/Vakor.DataBase.Lib/IndexAreas/Indexers/Indexer.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace Vakor.DataBase.Lib.IndexAreas.Indexers
+{
+    public class Indexer : IIndexer
+    {
+        public int StartKeyValue { get; set; }
+        public int EndKeyValue { get; set; }
+        public int ObjectBlockId { get; set; }
+
+        public Indexer()
+        {
+        }
+        
+        public Indexer(string indexerString)
+        {
+            var values = indexerString.Split(';').Take(3).Select(int.Parse).ToArray();
+            (StartKeyValue, EndKeyValue, ObjectBlockId) = (values[0], values[1], values[2]);
+        }
+
+        public string GenerateString()
+        {
+            string result = $"{StartKeyValue};{EndKeyValue};{ObjectBlockId}";
+            return result;
+        }
+    }
+}
