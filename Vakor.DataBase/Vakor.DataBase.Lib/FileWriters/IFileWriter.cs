7d4ff69113b0351ccdd8dde2e17f5365b9958b91
Vakor <myksv200@gmail.com>
2021-10-18 14:41:57 +0300

    18.10 FileWriter

---
+++ Vakor.DataBase/Vakor.DataBase.Lib/FileWriters/IFileWriter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Vakor.DataBase.Lib.FileWriters
+{
+    public interface IFileWriter
+    {
+        public void WriteToFile(IEnumerable<string> fileData, string fileName);
+    }
+}
