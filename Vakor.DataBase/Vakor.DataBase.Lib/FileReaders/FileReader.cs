3d54e40e0746503acbe7ac60383feddc906a3037
Vakor <myksv200@gmail.com>
2021-10-18 14:41:44 +0300

    18.10 FileReader

---
+++ Vakor.DataBase/Vakor.DataBase.Lib/FileReaders/FileReader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Vakor.DataBase.Lib.FileReaders
+{
+    public class FileReader : IFileReader
+    {
+        private readonly string _directoryPath;
+
+        public FileReader(string directoryPath)
+        {
+            _directoryPath = directoryPath;
+        }
+
+        public string[] ReadFile(string fileName)
+        {
+            string filePath = _directoryPath + "/" + fileName;
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException();
+            }
+
+            return File.ReadAllLines(filePath);
+        }
+    }
+}
